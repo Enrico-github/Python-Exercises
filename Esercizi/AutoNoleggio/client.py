43086c7b5c6ce9b5f2d7138b0b254ccd694dc1a8
Enrico Prearo <121225398+Enrico-github@users.noreply.github.com>
2026-03-02 06:50:11 +0000

    Update client.py

--- Esercizi/AutoNoleggio/client.py
+++ Esercizi/AutoNoleggio/client.py
@@ -6,12 +6,10 @@ def connetti_server():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect('localhost', 12345)
     return client
-
 def ricevi_auto(client):
-    data = client.recv(1024).decode()
+    data = client.recv(1024)
     lista = data.split(",")
     return lista
-
 def noleggia():
     auto = auto_var.get()
     giorni = entry_giorni.get()
@@ -22,24 +20,25 @@ def noleggia():
         messagebox.showerror("")
         return
     giorni = int(giorni)
-    if giorni == 0:
+    if giorni <= 0:
         messagebox.showerror("")
         return
     messaggio = auto + "," + str(giorni)
     client.sendall(messaggio.encode())
-    risposta = client.recv(1024).decode()
-    messagebox.showinfo("riepilogo", risposta)
+    ripsota = client.recv(1024).decode()
+    messagebox.showinfo("")
     entry_giorni.delete(0,tk.END)
-    
     client = connetti_server()
     lista_auto = ricevi_auto(client)
     root = tk.Tk()
-    root.title("")
+    root.title = ""
     auto_var = tk.StringVar(root)
     auto_var.set(lista_auto[0])
-    tk.Label(root, text = "inserisci giorni a noleggio").pack(pady = 5)
-    entry_giorni = tk.entry(root)
-    entry_giorni.pack(pady = 5)
-    tk.Button(root, text = "noleggio", command = noleggia).pack(pady = 10)
+    tk.Label(root, text = "").pack(pady=5)
+    tk.OptionMenu(root, auto_var, *lista_auto).pack(pady=5)
+    tk.Label(root, text = "").pack(pady=5)
+    entry_giorni = tk.Entry()
+    entry_giorni.pack(pady=5)
+    tk.Button(root, text = "", command = noleggia).pack(pady = 10)
     root.mainloop()
     client.close()
